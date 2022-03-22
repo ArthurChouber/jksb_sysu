0e5cd0cd71dd064f47862bf8214a5440b149ccb6
ArthurChouber <69112700+ArthurChouber@users.noreply.github.com>
2022-03-22 22:30:24 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,8 +1,1 @@
-import requests,json
-def read_json():
-    """
-    从config.json读取配置文件
-    """
-    with open("config.json",'r') as load_f:
-      load_dict = json.load(load_f)
-    return load_dict
+
